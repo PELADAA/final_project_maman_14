592301939a35e9b950c3c76df671150f21f22fb7
PELADAA <109914225+PELADAA@users.noreply.github.com>
2022-08-20 12:05:31 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,132 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "macro_deploy.h"
+#include "AsData.c"
+#include "Symbols.c"
+#include "directive.c"
+#include "Actions.c"
+#include "orders.c"
 
-#include "first_scan.h"
-
-char buffer[100];
-static int indexNew = -1;
-
-char* getCommand(FILE* fp1)/*Input: pointer to file. Output: char[81] that in the end of comman: /0. Discounts: file is open for reading*/
+int checkMortTxt(char* arrLine, int from)
 {
     char c;
-    char* arr;
-    int i;
-    if ((c = getNext(fp1)) != EOF)
-        setNext(c);
-    else
-        return NULL;
-    arr = (char*)malloc(commanSize * sizeof(char));
-    if (arr)
-    {
-        i = 0;
-        while ((c = getNext(fp1)) != EOF && c != '\n' && i < (commanSize - 1))
-        {
-            arr[i] = c;
-            i++;
-        }
-        arr[i] = '\0';
-        if (i <= (commanSize - 1) && c != EOF && c != '\n')
-        {
-            setNext(c);
-            while ((c = getNext(fp1)) != EOF && c != '\n')
-                ;
-            if (c == EOF)
-                setNext(c);
-            return "`Syntax error, too much long command.\0";
-        }
-        else
-            return arr;
-
-    }
-    else
-    {
-        return "`Dynamic allocatio error, can not create space.\0";
-    }
-}
-
-char getNext(FILE* fp1)/*Input: pointer to file. Output: next char. Discounts: file is open for reading*/
-{
-    if (indexNew > -1)
-    {
-        return buffer[indexNew--];
-    }
-    return getc(fp1);
-}
-
-void setNext(char c)/*Input: next char.*/
-{
-    buffer[++indexNew] = c;
-}
-
-int jumpSpace(char* command, int from)/*Input: pointer to arry, and starting poin. Output: point after starting text. Discounts: (1)*/
-{
-    char c;
-    while ((c = command[(from)]) != '\0' && isspace(c))
+    while (arrLine[from] != '\0' && isspace( arrLine[from]))
         from++;
-    return from;
+    return (arrLine[from] != '\0') ? 1 : 0;
 }
 
-int jumpBreak(char* command, int from)/*Input: pointer to arry, and starting poin. Output: point after comma, or -1 if not found Discounts: (1)*/
+int isItNumber(char* arrLine, int from)
 {
-    from = jumpSpace(command, from);
-    if (command[from] == ',')
-    {
-        from++;
-        return from;
-    }
-    else
-    {
-        return -1;
-    }
+	int start = from, end = 0;
+	if (start + end < 81 && (arrLine[start + end] == '-' || arrLine[start + end] == '+'))
+		start++;
+	while (arrLine[start + end] != '\0' && arrLine[start + end] >= '0' && arrLine[start + end] <= '9') {
+		end++;
+	}
+	return (end == 0) ? -1 : start + end;
 }
 
-int isTextLeft(char* command, int from)/*Input: pointer to arry, and starting poin. Output: if there is text left? 1 : 0. Discounts: (1)*/
+char* entryToEAE(char* arrLine, int from, int end)
 {
-    from = jumpSpace(command, from);
-    return (command[from] != '\0') ? 1 : 0;
-}
-
-int endOfText(char* command, int from)/*Input: pointer to arry, and starting poin. Output: point of end of text (comma, apostrophes or space). Discounts: (1)*/
-{
-    char c;
-    while ((c = command[(from)]) != '\0' && !isspace(c) && (c != ','))
-        from++;
-    return from;
-}
-
-char* clean_word(char* input_str) {
-
-    char* c;
-
-    int i = 0;
-
-    int j = 0;
-
-    //*c = input_str[i];
-    //strcpy(operand_str, input_str);
-    c = input_str;
-    *c = input_str[i];
-    //*c = (char)input_str;
-    //char* str_ptr = operand_str;
-
-    while (*c == SPACE) {
-        *c = (input_str[++i]);
-    }
-
-    //printf("++++ char is %c\n", *c);
-    while (isalnum(*c) || *c == '#' || *c == '-' || *c == ',' || *c == SPACE || *c == '.') {
-        if (*c == SPACE) {
-            *c = (input_str[++i]);
-            continue;
-        }
-        //printf("++++ char is %c\n", *c);
-
-        operand_str[j] = *c;
-        //printf("operand_str[j]: %c input_str[i]: %c\n", operand_str[j], input_str[i]);
-
-        *c = (input_str[++i]);
-        j++;
-        if (j > 80 || i > 80)
-            break;
-    }
-    operand_str[j] = '\0';
-    return operand_str;
+	static char name[30];
+	symbolPtr temp = Head_Symbol;
+	int lengthOfN = end - from + 1, i, result;
+	for (i = 0; i < lengthOfN; i++)
+	{
+		name[i] = arrLine[from + i];
+	}
+	name[i] = '\0';
+	while (temp)
+	{
+		if (strcmp(temp->name, name) == 0)
+		{
+			if (temp->type == 2)
+				return "`Syntax error, entery Symbol can't be external one.\0";
+			result = addEAE(&Head_EAE, temp->name, temp->adress, 0);
+			if (result == -2)
+				return "`Syntax error, Error in dynamic alocation.\0";
+			else
+				return NULL;
+		}
+		temp = temp->next;
+	}
+	return "`Syntax error, Symbol dosn't exist in hole file.\0";
 }
